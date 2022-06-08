c17c6696bddfc0636f03a0bcc3f04c128504dede
Carticss <diegogomez201094@hotmail.com>
2022-06-08 08:51:09 -0500

    Added the sprites, names and id for the list

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,5 +1,6 @@
 import { StyleSheet, Text, FlatList } from 'react-native'
 import React from 'react'
+import PokemonCard from './PokemonCard'
 
 export default function PokemonList(props) {
     
@@ -11,7 +12,7 @@ export default function PokemonList(props) {
             numColumns={2}
             showsVerticalScrollIndicator={false}
             keyExtractor={(pokemon) => String(pokemon.id)}
-            renderItem={({item}) => <Text>{item.name} </Text>}
+            renderItem={({item}) => <PokemonCard pokemon={item} />}
             contentContainerStyle={styles.flatListContentContainer}
         />
     )
